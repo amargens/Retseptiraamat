611ef05a7dbe6d041dc47e81950fa754233abb2b
amargens <a.margens@hotmail.com>
2018-03-17 02:59:54 +0200

    Footer'i uuendus. Lisan samuti logo assets kausta.

--- retseptiraamat/application/views/templates/footer.php
+++ retseptiraamat/application/views/templates/footer.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
-
-
-                <em>&copy; 2018</em>
+<div class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+    <span class="navbar-text">
+      <em>&copy;Retseptiraamat 2018</em>
+    </span>
+    </div>
+</div>
         </body>
 </html>
