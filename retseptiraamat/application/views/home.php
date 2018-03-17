705f71db0ff0f3569f422b3641558d0e9e443531
amargens <a.margens@hotmail.com>
2018-03-17 03:01:35 +0200

    Bootstrap'i kasutus klassidele ja siis tegin tabeli, mille sisse informatsioon panna. WCAG tahab, et paneks id ja header näitajad tabelisse.

--- retseptiraamat/application/views/home.php
+++ retseptiraamat/application/views/home.php
@@ -1,32 +1,54 @@
 <!DOCTYPE html>
 
-<div>
+<div class="col-md-6 col-md-offset-2">
     <?php
     echo "<h2>Siia tuleb retsepti raamatu koduleht.</h2>";
     ?>
 </div>
 
-<div>
-    <?php foreach ($recipes as $recipe_item): ?>
+<div class="table-responsive col-md-8 col-md-offset-2">
+    <table class="table">
+        <tr>
+        <td>
+        <thead>
+        <tr>
+            <th id="nimi">Nimi</th>
+            <th id="pilt">Pilt</th>
+            <th id="tekst">Tekst</th>
+            <th id="rohkem">Rohkem</th>
+        </tr>
+        </thead>
+        <tbody>
+                <div>
+            <?php foreach ($recipes as $recipe_item): ?>
+            <tr>
+            <td><?php echo $recipe_item['_title']; ?></td>
+            <td>
+                <?php if ( file_exists(APPPATH.'assets/recipe_img/'.$recipe_item['_imgpath'])) {
+                        $source = '../../application/assets/recipe_img/'.$recipe_item['_imgpath'];
+                        echo "<img src='".$source."' class='showImage' alt='Contect Image'>";
+                    }
+                    else {
+                        echo "<img src='../../application/assets/example_img.jpg' class='showImage' alt='Contect Image'>";
+                    }
 
-        <h3><?php echo $recipe_item['_title']; ?></h3>
-        <?php if ( file_exists(APPPATH.'assets/recipe_img/'.$recipe_item['_imgpath'])) {
-                $source = '../../application/assets/recipe_img/'.$recipe_item['_imgpath'];
-                echo "<img src='".$source."' class='showImage' alt='Contect Image'>";
-            }
-            else {
-                echo "<img src='../../application/assets/example_img.jpg' class='showImage' alt='Contect Image'>";
-            }
-            
-        
-        ?>
-        <div class="text">
-                <?php echo mb_substr($recipe_item['_text'], 0, 25) . "..."; ?>
-        </div>
-        
-        <p><?php echo anchor('Recipe/view/'.$recipe_item['_recipeID'], 'Loe Rohkem'); ?></p>
-        
 
-    <?php endforeach; ?>
+                ?>
+            </td>
+            <td>
+                <div class="text">
+                        <?php echo mb_substr($recipe_item['_text'], 0, 25) . "..."; ?>
+                </div>
+            </td>
+            <td>
+                <p><?php echo anchor('Recipe/view/'.$recipe_item['_recipeID'], 'Loe Rohkem'); ?></p>
+            </td>
+            </tr>
+            <?php endforeach; ?>
+                </div>
+        </tbody>
+        </td>
+        </tr>
+    </table>
 </div>
 
