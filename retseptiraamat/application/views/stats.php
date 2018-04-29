4b3ad1ba03405eeaad216b6c61dda44c6cb69687
amargens <a.margens@hotmail.com>
2018-04-29 03:21:44 +0300

    Aknasuuruste tugi parandus.

--- retseptiraamat/application/views/stats.php
+++ retseptiraamat/application/views/stats.php
@@ -3,29 +3,30 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <h2 data-tag="header" id="header"></h2>
-        
-        <div class="row">
-            <h4 data-tag="checkBrowser"></h4>
-            <h6 id="countBrowser"></h6>
-            <div id="checkBrowser">
-                
-            </div>
-        </div>
-        <div class="row">
-            <h4 data-tag="getTime"></h4>
-            <h6 id="countTime"></h6>
-            <div id="getTime">
-                
-            </div>
-        </div>
-        <div class="row">
-            <h4 data-tag="getpage"></h4>
-            <h6 id="countPage"></h6>
-            <div id="getpage">
-                
-            </div>
-        </div>
-    
     </div>
-    
 </div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <h4 data-tag="checkBrowser"></h4>
+        <h6 id="countBrowser"></h6>
+        <div id="checkBrowser">
+    </div>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <h4 data-tag="getTime"></h4>
+        <h6 id="countTime"></h6>
+        <div id="getTime">
+    </div>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <h4 data-tag="getpage"></h4>
+        <h6 id="countPage"></h6>
+        <div id="getpage">
+    </div>
+</div>
+</div>
+    
